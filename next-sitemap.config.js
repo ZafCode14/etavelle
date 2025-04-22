2d9f9eb7c33efe66ddc9240728ab876b4394a612
ZafCode14 <zafarwp14@gmail.com>
2025-04-22 05:20:03 +0200

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.etavelle.com',
+  generateRobotsTxt: true, // optional but recommended
+  sitemapSize: 5000, // optional
+};
