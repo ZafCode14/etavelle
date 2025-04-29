01cb248f6f1e8184f9b2bc727f9b573af66a791d
ZafCode14 <zafarwp14@gmail.com>
2025-04-29 11:20:02 +0300

    admin route only

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -27,25 +27,31 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
+  // Check if the path is a protected admin route
+  const isAdminRoute = request.nextUrl.pathname.startsWith('/admin')
+
+
   // Do not run code between createServerClient and
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
   // IMPORTANT: DO NOT REMOVE auth.getUser()
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+  if (isAdminRoute) {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser()
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone()
-    url.pathname = '/login'
-    return NextResponse.redirect(url)
+    if (
+      !user &&
+      !request.nextUrl.pathname.startsWith('/login') &&
+      !request.nextUrl.pathname.startsWith('/auth')
+    ) {
+      // no user, potentially respond by redirecting the user to the login page
+      const url = request.nextUrl.clone()
+      url.pathname = '/login'
+      return NextResponse.redirect(url)
+    }
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
