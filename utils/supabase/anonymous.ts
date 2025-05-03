1e429066d5a9dc5bf1e8aef2fb8045fe69b248b6
ZafCode14 <zafarwp14@gmail.com>
2025-05-03 21:08:42 +0300

    remove anonimous

--- utils/supabase/anonymous.ts
+++
@@ -1,7 +0,0 @@
-// utils/supabase/anonymous.ts
-import { createClient } from '@supabase/supabase-js'
-
-export const supabaseAnonymous = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
