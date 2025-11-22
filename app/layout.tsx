5602913740c7b4bbca9b368910cd91b3c3795e07
ZafCode14 <zafarwp14@gmail.com>
2025-11-22 07:12:09 +0200

    google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const mulish = Mulish({
   subsets: ["latin"],
@@ -12,7 +13,8 @@ const mulish = Mulish({
 
 export const metadata: Metadata = {
   title: "Etavelle | High-Performance, SEO-Optimized Websites That Convert",
-  description: "We build lightning-fast, SEO-optimized websites that rank higher, convert better, and grow with your brand — powered by Next.js.",
+  description:
+    "We build lightning-fast, SEO-optimized websites that rank higher, convert better, and grow with your brand — powered by Next.js.",
   icons: {
     icon: [
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
@@ -22,7 +24,8 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: "Etavelle | High-Performance, SEO-Optimized Websites That Convert",
-    description: "We build lightning-fast, SEO-optimized websites that rank higher, convert better, and grow with your brand — powered by Next.js.",
+    description:
+      "We build lightning-fast, SEO-optimized websites that rank higher, convert better, and grow with your brand — powered by Next.js.",
     url: "https://www.etavelle.com",
     siteName: "Etavelle",
     images: [
@@ -45,12 +48,23 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={mulish.className} suppressHydrationWarning>
         <Header />
         {children}
-        {/**
-         * 
-        <Chat />
-         */}
         <Footer />
-        <Toaster/>
+        <Toaster />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XZ4S3WCB71"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XZ4S3WCB71');
+          `}
+        </Script>
       </body>
     </html>
   );
